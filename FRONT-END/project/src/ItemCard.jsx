551243db17f618de281f8580f11a9f0b3941d3c5
Diego Mourão Oliveira <dii.mourao10@gmail.com>
2026-04-01 12:49:37 -0300

    feat(ui): ajustes no ItemCard
    - Padronizar proporção das imagens para 1:1
    - Alinhamento das imagens
    - Correção na estrutura do card para evitar que imagens de proporções diferentes quebrem o layout

--- FRONT-END/project/src/ItemCard.jsx
+++ FRONT-END/project/src/ItemCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 //  PROPS
 //  img = src da imagem
@@ -8,22 +8,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ItemCard = (props) => {
   return (
-    <div className="card h-100 shadow-sm hover rounded-4" style={{
-      transition: "transform 0.2s, box-shadow 0.2s",
-      cursor: "pointer"
-    }}
+    <div
+      className="card h-100 shadow-sm hover rounded-4"
+      style={{
+        transition: "transform 0.2s, box-shadow 0.2s",
+        cursor: "pointer",
+      }}
     >
-      <img 
-        className="card-img-top"  
-        src={props.img} 
-        alt={props.itemName}
+      <div
+        className="card-image-container d-flex"
         style={{
-          objectFit: "cover",
-          padding: "0.75rem"
+          aspectRatio: "1/1",
+          alignItems: "center",
+          justifyContent: "center",
         }}
-      />
+      >
+        <img
+          className="card-img-top"
+          src={props.img}
+          alt={props.itemName}
+          style={{
+            objectFit: "cover",
+            padding: "0.75rem",
+          }}
+        />
+      </div>
       <div className="card-body d-flex flex-column text-center rounded-bottom-4 custom-body item-card ">
-        <h2 className="card-title custom-title m-auto item-card-title">{props.itemName}</h2>
+        <h2 className="card-title custom-title m-auto item-card-title">
+          {props.itemName}
+        </h2>
       </div>
     </div>
   );
