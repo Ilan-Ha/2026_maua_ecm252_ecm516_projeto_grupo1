2d4d162d71b04fc4f5cd54454fe11a03cd8ee4ef
Ilan Hameiry <Ilan.hame@gmail.com>
2026-05-20 20:56:52 -0300

    feat(gateway): adiciona padra de resposta

--- arquitetura-microservicos/back-end/gateway/index.js
+++ arquitetura-microservicos/back-end/gateway/index.js
@@ -22,44 +22,40 @@ app.use(cors());
 // Permite receber JSON direto no req.body
 app.use(express.json());
 
+// dados de retorno
+
+const returnData = (response) => {
+    const {content, message} = response.data
+    const status = response.status
+
+    console.log(`Status: ${status}\nContent: ${content}\nMessage: ${message}`)
+
+    return {status, content, message}
+}
+
 // Rota de cadastro
 app.post(path.auth.register, async (req, res) => {
-    /*
-    Registro
-    --------
-    envia requisição para servidor de autentificação
-    se aceitar -> autentificação envia para gateway requisição do servidor de usuario (fetch gateway/usuario/adicionar)
-    gateway recebe faz um post para adicionar usuario
-    gateway enviar solicitação para server de usuario
-    server de usuario recebe solicitacao, processa e retorna
-    gateway devolve retorno ao server de autentificacao
-    se for positivo, registra o novo usuario
-    gateway retorna ao front*/
-
-    console.log(req.body)
     
     const {event, payload} = req.body
     console.log(event)
     console.log(payload)
 
-    await axios.post(`${config.url}:${svc.auth}${path.auth.register}`, {
+    const response = await axios.post(`${config.url}:${svc.auth}${path.auth.register}`, {
       event: event,
       payload: payload
     })
-    res.end()
-});
 
+    const {status, content, message} = returnData(response)
+
+    res.status(status).json({
+      content: content,
+      message: message
+    })
+});
 
 // Inicialização do servidor
 const startServer = async () => {
   try {
-    // se for usar o .env e precisar de uma logica de verificacao
-    // if (!process.env.MONGO_URI) {
-    //   throw new Error("MONGO_URI não definida no .env");
-    // }
-    // await mongoose.connect(process.env.MONGO_URI);
-    // console.log("Mongo conectado");
-    // await initSeed();
     app.listen(PORT, () => {
       console.log(`Rodando em ${config.url}:${PORT}`);
     });
